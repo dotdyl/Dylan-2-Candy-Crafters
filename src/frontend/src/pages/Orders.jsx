a898c2e994504d0e1b5d9c6a9ebad2ede86add1f
slaiff <dylan.keyhantaj@gmail.com>
2026-05-07 01:47:04 -0700

    fixed improper import naming

--- src/frontend/src/pages/Orders.jsx
+++ src/frontend/src/pages/Orders.jsx
@@ -1,30 +1,25 @@
-import { useState, useEffect } from 'react';  // Importing useState for managing state in the component
-import TableRow from '../components/TableRow';
-import CreateOrderForm from '../components/CRUD_Orders/CreateOrder';
-
-
+import { useState, useEffect } from "react"; // Importing useState for managing state in the component
+import TableRow from "../components/TableRow";
+import CreateOrderForm from "../components/CRUD_Orders/FormCreateOrder";
 
 function Orders({ backendURL }) {
-
     // Set up a state variable `people` to store and display the backend response
     const [orders, setOrders] = useState([]);
 
     const getData = async function () {
         try {
             // Make a GET request to the backend
-            const response = await fetch(backendURL + '/orders');
+            const response = await fetch(backendURL + "/orders");
 
             // Convert the response into JSON format
             const { orders } = await response.json();
 
             // Update the people state with the response data
             setOrders(orders);
-
         } catch (error) {
             // If the API call fails, print the error to the console
             console.log(error);
         }
-
     };
 
     // Load table on page load
@@ -39,21 +34,27 @@ function Orders({ backendURL }) {
             <table>
                 <thead>
                     <tr>
-                        {orders.length > 0 && Object.keys(orders[0]).map((header, index) => (
-                            <th key={index}>{header}</th>
-                        ))}
+                        {orders.length > 0 &&
+                            Object.keys(orders[0]).map((header, index) => (
+                                <th key={index}>{header}</th>
+                            ))}
                         <th></th>
                     </tr>
                 </thead>
 
                 <tbody>
                     {orders.map((candy, index) => (
-                        <TableRow key={index} rowObject={candy} backendURL={backendURL} refreshOrders={getData} />
+                        <TableRow
+                            key={index}
+                            rowObject={candy}
+                            backendURL={backendURL}
+                            refreshOrders={getData}
+                        />
                     ))}
-
                 </tbody>
             </table>
             <CreateOrderForm></CreateOrderForm>
         </>
-    )
-} export default Orders;
+    );
+}
+export default Orders;
