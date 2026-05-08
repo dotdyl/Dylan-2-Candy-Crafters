aad556faed4e3c3128dd9fc28b14100c413d4aa3
slaiff <dylan.keyhantaj@gmail.com>
2026-05-07 17:18:57 -0700

    finished dml

--- DML.sql
+++ DML.sql
@@ -24,11 +24,44 @@ SELECT * FROM Orders;
 UPDATE Orders
 SET vendorID = @vendorIDINPUT, subTotal = @subTotal, taxAmtOfTotal = @taxAmtOfTotal,
 shippingCost = @shippingCost, orderDate = @orderDate, totalDue = @totalDue
-WHERE orderID = @orderIDInput
+WHERE orderID = @orderIDInput;
 
 DELETE FROM Orders
 WHERE orderID = @orderIDINPUT;
 
-
 -- CRUD Vendors
+
+INSERT INTO Vendors (vendorName, addressLine1, addressLine2, city, state, postalCode, ytdPurchasesTotal)
+VALUES (@vendorNameINPUT, @addressLine1, @addressLine2, @city, @state, @postalCode, @ytdPurchasesTotal);
+
+SELECT * FROM Vendors;
+
+UPDATE Vendors
+SET vendorName = @vendorNameINPUT,
+    addressLine1 = @addressLine1,
+    addressLine2 = @addressLine2,
+    city = @city,
+    state = @state,
+    postalCode = @postalCode,
+    ytdPurchasesTotal = @ytdPurchasesTotal
+WHERE vendorID = @vendorIDINPUT;
+
+DELETE FROM Vendors
+WHERE vendorID = @vendorIDINPUT;
+
 -- CRUD InventorySpaces
+
+INSERT INTO InventorySpaces (candyID, lastStocked, gallonsFilled)
+VALUES (@candyIDINPUT, @lastStockedINPUT, @gallonsFilledINPUT);
+
+SELECT * FROM InventorySpaces;
+
+UPDATE InventorySpaces
+SET candyID = @candyIDInput,
+    lastStocked = @lastStockedInput,
+    gallonsFilled = @gallonsFilledInput
+WHERE inventoryID = @inventoryIDInput;
+
+-- Query to delete an inventory space
+DELETE FROM InventorySpaces
+WHERE inventoryID = @inventoryIDInput;
