785acbfb78852696885355fae53db7cdf6a5e5ad
dotdyl <dotdyldev@gmail.com>
2026-05-08 21:28:26 -0700

    Fixes to Allow for Production Run

--- src/frontend/reactServer.cjs
+++ src/frontend/reactServer.cjs
@@ -1,14 +1,17 @@
 const express = require('express');
 const path = require('path');
+const dotenv = require('dotenv')
+
+dotenv.config({path : './.env'})
 
 const app = express();
 app.use(express.static(path.join(__dirname, 'dist')));
 
-const PORT = import.meta.env.VITE_FRONTEND_PORT;
+const PORT = process.env.VITE_FRONTEND_PORT;
 
 // Handles any requests that don't match the ones above to return the React app
 // A request to '/nonExist' will redirect to the index.html where react router takes over at '/'
-app.get('*', (req, res) => {
+app.use((req, res) => {
     res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
 });
 
