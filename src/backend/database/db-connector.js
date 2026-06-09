4635b70e5535bc121938dfdcf2f86e929770cd9a
dotdyl <dotdyldev@gmail.com>
2026-06-08 22:42:43 -0700

    Reverted selective order details viewing due to bugs, Obscrued database connection

--- src/backend/database/db-connector.js
+++ src/backend/database/db-connector.js
@@ -2,18 +2,28 @@
 // Date: 05/28/2026
 // Based on:
 // Source URL: https://canvas.oregonstate.edu/courses/2042369/assignments/10464666?module_item_id=26640209
+const path = require('path');
+const dotenv = require('dotenv')
+
+const result = dotenv.config({
+    path: path.resolve(__dirname, './.env')
+});
 
 // Get an instance of mysql we can use in the app
 let mysql = require('mysql2')
+
+const db_user = process.env.DB_USER
+const db_password = process.env.DB_PASSWORD
+const db_database = process.env.DB_DATABASE
 
 // Create a 'connection pool' using the provided credentials
 const pool = mysql.createPool({
     waitForConnections: true,
     connectionLimit   : 10,
     host              : 'classmysql.engr.oregonstate.edu',
-    user              : 'cs340_knappdy',
-    password          : '3993',
-    database          : 'cs340_knappdy'
+    user              : db_user,
+    password          : db_password,
+    database          : db_database
 }).promise(); // This makes it so we can use async / await rather than callbacks
 
 // Export it for use in our application
--- src/frontend/src/pages/Orders.jsx
+++ src/frontend/src/pages/Orders.jsx
@@ -83,20 +83,9 @@ function Orders({ backendURL }) {
                 </div>
             </div>
 
-            <h1 className='text-xl font-bold text-base-content'>View Order Details for an Order</h1>
-            <div className="form-control w-full max-w-[250px]">
-                <select name="viewOrderDetailsByOrder" id="viewOrderDetailsByOrder" className="select select-bordered select-primary select-sm w-full" defaultValue="" onChange={e => {console.log(e.target.value); setOrderDetailsToView(JSON.parse(e.target.value))}}>
-                    <option  disabled hidden value="">Select an Order</option>
-                    {orders.map((order) => (
-                        <option key={order.orderId} value={JSON.stringify(order.orderId)}>
-                            {order.orderId}
-                        </option>
-                    ))}
-                </select>
-            </div>
-
-            {orderDetailsToView != 0 && <div className='card bg-base-100 max-w-full border border-base-300 shadow-md mb-6'>
+           <div className='card bg-base-100 max-w-full border border-base-300 shadow-md mb-6'>
                 <div className='card-body'>
+                    <h1 className='text-xl font-bold text-base-content'>All Order Details</h1>
                     <table className='table table-zebra'>
                         <thead className='text-base text-primary'>
                             <tr>
@@ -110,7 +99,7 @@ function Orders({ backendURL }) {
 
                         <tbody>
                             {/*Use '...' to SPREAD the detail OBJ across its pieces/subfields, one of these is candyID, we now can change it dynamically.*/}
-                            {(orderDetails.filter((detail) => (detail.orderId === orderDetailsToView)).map((detail, index) => (
+                            {(orderDetails.map((detail, index) => (
                                 <TableRow
                                     key={index}
                                     rowObject={{
@@ -125,7 +114,7 @@ function Orders({ backendURL }) {
                         </tbody>
                     </table>
                 </div>
-            </div>}
+            </div>
             <CreateOrderForm backendURL={backendURL} vendors={vendors} candies={candies} orders={orders}></CreateOrderForm>
             {/*Not necessary, probably, but the form was made: <UpdateOrderForm vendors={vendors} candies={candies} orders={orders} orderDetails={orderDetails}></UpdateOrderForm>*/
             /*
